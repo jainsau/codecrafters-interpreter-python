c65afe53092ef64050808a967c22ab923dd9eb33
Saurabh Jain <saurabh_jain2011@pgp.isb.edu>
2024-10-20 12:05:58 +0530

    Scanner: Refactored code

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import sys
-import re
-from app.scanner import scanner
+from app.scanner import tokenize
 
 
 def main():
@@ -18,105 +17,14 @@ def main():
     with open(filename) as file:
         file_contents = file.read()
 
-    # Uncomment this block to pass the first stage
     if file_contents:
-        line, output, errors = 1, "", ""
-        i = 0
-        while i < len(file_contents):
-            token = file_contents[i]
-            if (i + 1) < len(file_contents) and file_contents[i : i + 2] in [
-                "==",
-                "!=",
-                "<=",
-                ">=",
-            ]:
-                res, _ = scanner(file_contents[i : i + 2])
-                output = output + res if res != "" else output
-                i += 2
-                continue
-            elif (i + 1) < len(file_contents) and file_contents[i : i + 2] == "//":
-                while i < len(file_contents) and file_contents[i] != "\n":
-                    i += 1
-                line += 1
-            elif token == "\n":
-                line += 1
-            elif token in [" ", "\t"]:
-                pass
-            elif token == '"':
-                res, err = "", ""
-                i += 1
-                while True:
-                    if i == len(file_contents) or file_contents[i] == "\n":
-                        res, err = "", f"[line {line}] Error: Unterminated string.\n"
-                        line += 1
-                        break
-                    elif file_contents[i] == '"':
-                        break
-                    else:
-                        res += file_contents[i]
-                    i += 1
-                errors += err
-                output = output + f'STRING "{res}" {res}\n' if res != "" else output
-            elif token in "0123456789":
-                res = token
-                i += 1
-                while i < len(file_contents):
-                    if file_contents[i] in ".0123456789":
-                        res += file_contents[i]
-                    else:
-                        break
-                    i += 1
-                if "." in res:
-                    res_ = res
-                    # drop extra zeros past decimal
-                    while res.endswith("0"):
-                        res = res[:-1]
-                    if res.endswith("."):
-                        res += "0"
-                    output = output + f"NUMBER {res_} {res}\n"
-                else:
-                    output = output + f"NUMBER {res} {res}.0\n"
-                continue
-            elif re.search("[A-z_]", token):
-                res = re.search("^[A-z_][A-z_0-9]*", file_contents[i:]).group()
-                if res in [
-                    "and",
-                    "class",
-                    "else",
-                    "false",
-                    "for",
-                    "fun",
-                    "if",
-                    "nil",
-                    "or",
-                    "print",
-                    "return",
-                    "super",
-                    "this",
-                    "true",
-                    "var",
-                    "while",
-                ]:
-                    res_ = res.upper()
-                    output += f"{res_} {res} null\n"
-                else:
-                    output += f"IDENTIFIER {res} null\n"
-                i += len(res)
-                continue
-            else:
-                res, err = scanner(token)
-                output = output + res if res != "" else output
-                errors = errors + f"[line {line}] {err}" if err != "" else errors
-            i += 1
-        output += "EOF  null"
+        output, errors = tokenize(file_contents)
         print(errors, file=sys.stderr)
         print(output)
         if errors != "":
             exit(65)
     else:
-        print(
-            "EOF  null"
-        )  # Placeholder, remove this line when implementing the scanner
+        print("EOF  null")
 
 
 if __name__ == "__main__":
--- app/scanner.py
+++ app/scanner.py
@@ -1,44 +1,132 @@
-def scanner(c: str) -> str:
-    match c:
-        case "(":
-            return "LEFT_PAREN ( null\n", ""
-        case ")":
-            return "RIGHT_PAREN ) null\n", ""
-        case "{":
-            return "LEFT_BRACE { null\n", ""
-        case "}":
-            return "RIGHT_BRACE } null\n", ""
-        case ",":
-            return "COMMA , null\n", ""
-        case ".":
-            return "DOT . null\n", ""
-        case "-":
-            return "MINUS - null\n", ""
-        case "+":
-            return "PLUS + null\n", ""
-        case ";":
-            return "SEMICOLON ; null\n", ""
-        case "/":
-            return "SLASH / null\n", ""
-        case "*":
-            return "STAR * null\n", ""
-        case "=":
-            return "EQUAL = null\n", ""
-        case "==":
-            return "EQUAL_EQUAL == null\n", ""
-        case "!":
-            return "BANG ! null\n", ""
-        case "!=":
-            return "BANG_EQUAL != null\n", ""
-        case "<":
-            return "LESS < null\n", ""
-        case ">":
-            return "GREATER > null\n", ""
-        case "<=":
-            return "LESS_EQUAL <= null\n", ""
-        case ">=":
-            return "GREATER_EQUAL >= null\n", ""
-        case "/":
-            return "SLASH / null\n", ""
+import re
+
+
+def tokenize(text: str) -> (str, str):
+    i, line, output, errors = 0, 1, "", ""
+    while i < len(text):
+        l_token, line, lexeme, error = scanner(text[i:], line)
+        output += lexeme
+        errors += error
+        i += l_token
+    output += "EOF  null"
+
+    return output, errors
+
+
+def scanner(text: str, line: int) -> (int, bool, str, str):
+    lexeme, error = "", ""
+    match text:
+        case _ if (m := re.match(r"[A-z_][\w]*", text)):
+            t = m.group()
+            if t in [
+                "and",
+                "class",
+                "else",
+                "false",
+                "for",
+                "fun",
+                "if",
+                "nil",
+                "or",
+                "print",
+                "return",
+                "super",
+                "this",
+                "true",
+                "var",
+                "while",
+            ]:
+                lexeme = f"{t.upper()} {t} null\n"
+            else:
+                lexeme = f"IDENTIFIER {t} null\n"
+        case _ if (m := re.match(r"[\n]+", text)):
+            # match newline character(s)
+            t = m.group()
+            line += len(t)
+        case _ if (m := re.match(r"[ \t]+", text)):
+            # match contiguous tabs and spaces
+            t = m.group()
+            pass
+        case _ if (m := re.match(r"//.+", text)):
+            # match comment
+            t = m.group()
+        case _ if (m := re.match(r"\"([^\"]*)\"", text)):
+            # match strings
+            t = m.group()
+            lexeme = f"STRING {t} {m.group(1)}\n"
+        case _ if (m := re.match(r"\"[^\"\n]*", text)):
+            # match unbalanced strings (error)
+            t = m.group()
+            error = f"[line {line}] Error: Unterminated string.\n"
+        case _ if (m := re.match(r"([\d]+)\.?([\d]*)", text)):
+            # match number literals
+            # handle trailing zeros sensibly
+            t = m.group()
+            if (fraction := re.sub(r"0+$", r"", f"{m.group(2)}")) == "":
+                fraction = "0"
+            lexeme = f"NUMBER {t} {m.group(1)}.{fraction}\n"
+        case _ if (m := re.match(r"==", text)):
+            t = m.group()
+            lexeme = f"EQUAL_EQUAL {t} null\n"
+        case _ if (m := re.match(r"!=", text)):
+            t = m.group()
+            lexeme = f"BANG_EQUAL {t} null\n"
+        case _ if (m := re.match(r"<=", text)):
+            t = m.group()
+            lexeme = f"LESS_EQUAL {t} null\n"
+        case _ if (m := re.match(r">=", text)):
+            t = m.group()
+            lexeme = f"GREATER_EQUAL {t} null\n"
+        case _ if (m := re.match(r"\(", text)):
+            t = m.group()
+            lexeme = f"LEFT_PAREN {t} null\n"
+        case _ if (m := re.match(r"\)", text)):
+            t = m.group()
+            lexeme = f"RIGHT_PAREN {t} null\n"
+        case _ if (m := re.match(r"{", text)):
+            t = m.group()
+            lexeme = f"LEFT_BRACE {t} null\n"
+        case _ if (m := re.match(r"}", text)):
+            t = m.group()
+            lexeme = f"RIGHT_BRACE {t} null\n"
+        case _ if (m := re.match(r",", text)):
+            t = m.group()
+            lexeme = f"COMMA {t} null\n"
+        case _ if (m := re.match(r"\.", text)):
+            t = m.group()
+            lexeme = f"DOT {t} null\n"
+        case _ if (m := re.match(r"-", text)):
+            t = m.group()
+            lexeme = f"MINUS {t} null\n"
+        case _ if (m := re.match(r"\+", text)):
+            t = m.group()
+            lexeme = f"PLUS {t} null\n"
+        case _ if (m := re.match(r";", text)):
+            t = m.group()
+            lexeme = f"SEMICOLON {t} null\n"
+        case _ if (m := re.match(r"/", text)):
+            t = m.group()
+            lexeme = f"SLASH {t} null\n"
+        case _ if (m := re.match(r"\*", text)):
+            t = m.group()
+            lexeme = f"STAR {t} null\n"
+        case _ if (m := re.match(r"=", text)):
+            t = m.group()
+            lexeme = f"EQUAL {t} null\n"
+        case _ if (m := re.match(r"!", text)):
+            t = m.group()
+            lexeme = f"BANG {t} null\n"
+        case _ if (m := re.match(r"<", text)):
+            t = m.group()
+            lexeme = f"LESS {t} null\n"
+        case _ if (m := re.match(r">", text)):
+            t = m.group()
+            lexeme = f"GREATER {t} null\n"
+        case _ if (m := re.match(r"/", text)):
+            t = m.group()
+            lexeme = f"SLASH {t} null\n"
         case _:
-            return "", f"Error: Unexpected character: {c}\n"
+            t = text[0]
+            error = f"[line {line}] Error: Unexpected character: {text[0]}\n"
+
+    return len(t), line, lexeme, error
