25f493d92c26a3c3ad3717db725e03249eb628af
Saurabh Jain <saurabh_jain2011@pgp.isb.edu>
2024-12-17 04:36:47 +0530

    codecrafters submit [skip ci]

--- app/expr.py
+++ app/expr.py
@@ -13,11 +13,41 @@ class Expr(abc.ABC):
 
 
 class Visitor(Protocol[T]):
+    # def visit_assign_expr(self, expr: "Assign") -> T: ...
+    #
     def visit_binary_expr(self, expr: "Binary") -> T: ...
 
+    #
+    # def visit_call_expr(self, expr: "Call") -> T: ...
+    #
+    # def visit_get_expr(self, expr: "Get") -> T: ...
+    #
     def visit_grouping_expr(self, expr: "Grouping") -> T: ...
 
     def visit_literal_expr(self, expr: "Literal") -> T: ...
+
+    #
+    # def visit_logical_expr(self, expr: "Logical") -> T: ...
+    #
+    # def visit_set_expr(self, expr: "Set") -> T: ...
+    #
+    # def visit_super_expr(self, expr: "Super") -> T: ...
+    #
+    # def visit_this_expr(self, expr: "This") -> T: ...
+    #
+    # def visit_unary_expr(self, expr: "Unary") -> T: ...
+    #
+    # def visit_variable_expr(self, expr: "Variable") -> T: ...
+    #
+
+
+# class Assign(Expr):
+#     def __init__(self, name: ValidToken, value: Expr):
+#       self.name = name
+#         self.value = value
+#
+#     def accept(self, visitor: Visitor[T]) -> T:
+#         return visitor.visit_assign_expr(self)
 
 
 class Binary(Expr):
@@ -28,6 +58,26 @@ class Binary(Expr):
 
     def accept(self, visitor: Visitor[T]) -> T:
         return visitor.visit_binary_expr(self)
+
+
+# class Call(Expr):
+#     def __init__(self, callee: Expr, paren: ValidToken, arguments: List[Expr]):
+#         self.callee = callee
+#         self.paren = paren
+#         self.arguments = arguments
+#
+#     def accept(self, visitor: Visitor[T]) -> T:
+#         return visitor.visit_call_expr(self)
+#
+#
+# class Get(Expr):
+#     def __init__(self, obj: Expr, name: ValidToken):
+#         self.obj = obj
+#         self.name = name
+#
+#     def accept(self, visitor: Visitor[T]) -> T:
+#         return visitor.visit_get_expr(self)
+#
 
 
 class Grouping(Expr):
@@ -46,6 +96,43 @@ class Literal(Expr):
         return visitor.visit_literal_expr(self)
 
 
+# class Logical(Expr):
+#     def __init__(self, left: Expr, operator: ValidToken, right: Expr):
+#         self.left = left
+#         self.operator = operator
+#         self.right = right
+#
+#     def accept(self, visitor: Visitor[T]) -> T:
+#         return visitor.visit_logical_expr(self)
+#
+#
+# class Set(Expr):
+#     def __init__(self, obj: Expr, name: ValidToken, value: Expr):
+#         self.obj = obj
+#         self.name = name
+#         self.value = value
+#
+#     def accept(self, visitor: Visitor[T]) -> T:
+#         return visitor.visit_set_expr(self)
+#
+#
+# class Super(Expr):
+#     def __init__(self, keyword: ValidToken, method: ValidToken):
+#         self.keyword = keyword
+#         self.method = method
+#
+#     def accept(self, visitor: Visitor[T]) -> T:
+#         return visitor.visit_super_expr(self)
+#
+#
+# class This(Expr):
+#     def __init__(self, keyword: ValidToken):
+#         self.keyword = keyword
+#
+#     def accept(self, visitor: Visitor[T]) -> T:
+#         return visitor.visit_this_expr(self)
+#
+#
 class Unary(Expr):
     def __init__(self, operator: ValidToken, right: Expr):
         self.operator = operator
@@ -53,3 +140,13 @@ class Unary(Expr):
 
     def accept(self, visitor: Visitor[T]) -> T:
         return visitor.visit_unary_expr(self)
+
+
+#
+#
+# class Variable(Expr):
+#     def __init__(self, name: ValidToken):
+#         self.name = name
+#
+#     def accept(self, visitor: Visitor[T]) -> T:
+#         return visitor.visit_variable_expr(self)
--- app/interpreter.py
+++ app/interpreter.py
@@ -1,4 +1,4 @@
-from .expr import Visitor, Expr, Literal
+from .expr import Visitor, Expr, Grouping, Binary, Unary, Literal
 from .scanner import ValidTokenType
 
 
@@ -6,7 +6,61 @@ class Interpreter(Visitor):
     def evaluate(self, expr: Expr) -> object:
         return expr.accept(self)
 
+    def is_truthy(self, obj: object):
+        if obj is None:
+            return False
+        if isinstance(obj, bool):
+            return obj
+        return True
+
+    def is_equal(a: object, b: object) -> bool:
+        if (a is None) ^ (b is None):
+            return True
+
+        return a == b
+
     def visit_literal_expr(self, expr: Literal) -> object:
         if expr.value.type is ValidTokenType.INTEGER:
             return int(float(expr.value.literal))
         return expr.value.literal if expr.value.literal else expr.value.lexeme
+
+    def visit_grouping_expr(self, expr: Grouping) -> object:
+        return self.evaluate(expr.expression)
+
+    def visit_unary_expr(self, expr: Unary) -> object:
+        right = self.evaluate(expr.right)
+
+        match expr.operator.type:
+            case ValidTokenType.MINUS:
+                return -float(right)
+            case ValidTokenType.BANG:
+                return not self.is_truthy(right)
+
+    def visit_binary_expr(self, expr: Binary) -> str:
+        left = self.evaluate(expr.left)
+        right = self.evaluate(expr.right)
+
+        match expr.operator.type:
+            case ValidTokenType.GREATER:
+                return left > right
+            case ValidTokenType.GREATER_EQUAL:
+                return left >= right
+            case ValidTokenType.LESS:
+                return left < right
+            case ValidTokenType.LESS_EQUAL:
+                return left <= right
+            case ValidTokenType.BANG_EQUAL:
+                return not self.is_equal(left, right)
+            case ValidTokenType.EQUAL_EQUAL:
+                return self.is_equal(left, right)
+            case ValidTokenType.MINUS:
+                return float(left) - float(right)
+            case ValidTokenType.PLUS:
+                if isinstance(left, float) and isinstance(right, float):
+                    return left + right
+                elif isinstance(left, str) and isinstance(right, str):
+                    return left + right
+            case ValidTokenType.SLASH:
+                return float(left) / float(right)
+            case ValidTokenType.STAR:
+                return float(left) * float(right)
